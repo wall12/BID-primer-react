3c85fe6967bc560ba8cb1123ce80cb61b8b2ef78
wall12 <espinola.walter512@gmail.com>
2023-01-16 02:51:50 -0300

    Ejercicio Core Primer react con JSX

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="titulo">Hello Dojo!</h1>
+      <h2 className="sub-titulo">Things I need to do:</h2>
+      <ul className="lista">
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
